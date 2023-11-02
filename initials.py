348b0b6c6fc48f5665fc4b2a420ace74110ddc9a
Mr-pluffen <149428756+Mr-pluffen@users.noreply.github.com>
2023-11-01 22:56:26 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+# I love sports
+
+print('PPPP              A')
+print('P   P           A   A')
+print('PPPP           A     A')
+print('P             A  A A  A')
+print('P            A         A')
+print('P           A           A')
